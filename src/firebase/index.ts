502d744d8dd545d127db57bf2acb889bfae3550c
Hitesh Rajpurohit <hitesh18113@gmail.com>
2025-12-20 08:55:08 +0000

    File changes

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -5,7 +5,7 @@ import { getFirestore, Firestore } from 'firebase/firestore';
 
 // IMPORTANT: Do not directly edit this object.
 // Your Firebase project's configuration will be injected here automatically.
-const firebaseConfig = {"apiKey":"your-api-key","authDomain":"your-project-id.firebaseapp.com","projectId":"your-project-id","storageBucket":"your-project-id.appspot.com","messagingSenderId":"your-sender-id","appId":"your-app-id"};
+const firebaseConfig = {"projectId":"studio-4040792947-1cce6","appId":"1:797073609616:web:301ca46b5b9d7939f65915","apiKey":"AIzaSyCD07jFZ8LgVlh7lkkoCkQDq_T7dJeb_7I","authDomain":"studio-4040792947-1cce6.firebaseapp.com","storageBucket":"studio-4040792947-1cce6.appspot.com","messagingSenderId":"797073609616"};
 
 
 function initializeFirebase(): { app: FirebaseApp; auth: Auth; firestore: Firestore } {
@@ -21,4 +21,5 @@ export { initializeFirebase };
 export * from './provider';
 export * from './auth/use-user';
 export * from './client-provider';
+
 
